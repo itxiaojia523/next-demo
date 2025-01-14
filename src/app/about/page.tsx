75cbca2d667eb0046bb674086ff2932203c17a2f
xiaojia <chenminxin51@gmail.com>
2025-01-14 16:27:30 +0900

    useRouter: 实现路由跳转

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,9 +3,23 @@
  * 必须命名为page.tsx或page.js
  * 默认暴露
  */
-
+"use client";
+import { useRouter } from "next/navigation";
 const About = () => {
-  return <div>About</div>;
+  const router = useRouter();
+  return (
+    <div>
+      <h1>About</h1>
+      <button
+        onClick={() => {
+          router.push("/");
+        }}
+        className="bg-blue-500 text-white p-2 rounded-md"
+      >
+        Go home
+      </button>
+    </div>
+  );
 };
 
 export default About;
