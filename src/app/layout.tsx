3e716e02ad77d4e3b3a7bda09a8e5dd7ec818d05
xiaojia <chenminxin51@gmail.com>
2025-01-14 16:14:02 +0900

    layout: shared UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="bg-slate-900 text-white p-4 text-center">
+          <p>Welcome to Next15</p>
+        </header>
         {children}
+        <footer className="bg-slate-900 text-white p-4 text-center">
+          Codevolution
+        </footer>
       </body>
     </html>
   );
