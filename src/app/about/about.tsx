53d2ab33f9f3742a74abccdf3f9534c83f6e113d
xiaojia <chenminxin51@gmail.com>
2025-01-17 20:06:22 +0900

    metadata-title

---
+++ src/app/about/about.tsx
@@ -0,0 +1,26 @@
+/*
+ * app内的文件夹名即路径名
+ * 必须命名为page.tsx或page.js
+ * 默认暴露
+ */
+"use client";
+import { useRouter } from "next/navigation";
+
+const About = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>About</h1>
+      <button
+        onClick={() => {
+          router.push("/");
+        }}
+        className="bg-blue-500 text-white p-2 rounded-md"
+      >
+        Go home
+      </button>
+    </div>
+  );
+};
+
+export default About;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,33 +1,12 @@
-/*
- * app内的文件夹名即路径名
- * 必须命名为page.tsx或page.js
- * 默认暴露
- */
-"use client";
-import { useRouter } from "next/navigation";
+import { Metadata } from "next";
+import About from "./about";
 
 // 注意不能再client component用 metadata
-// export const metadata = {
-//   title: "dcos",
-//   description: "dcos",
-// };
 // 解决方案，page里移除use client
 // 这边代码全部移动到about.tsx 再引入
-const About = () => {
-  const router = useRouter();
-  return (
-    <div>
-      <h1>About</h1>
-      <button
-        onClick={() => {
-          router.push("/");
-        }}
-        className="bg-blue-500 text-white p-2 rounded-md"
-      >
-        Go home
-      </button>
-    </div>
-  );
+export const metadata: Metadata = {
+  title: {
+    absolute: "About", //可以覆盖父组件的title，父组件的template都不生效，绝对是About的意思
+  },
 };
-
 export default About;
