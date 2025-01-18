0b431445bdcf6544267d9e7982f194f1b8e177a3
xiaojia <chenminxin51@gmail.com>
2025-01-18 23:27:58 +0900

    Recovering from Errors

--- src/app/products/error.tsx
+++ src/app/products/error.tsx
@@ -1,5 +1,26 @@
 "use client";
 
-export default function ErrorBoundary({ error }: { error: Error }) {
-  return <div>{error.message}</div>;
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset(); // 只尝试re-render client side  所以要尝试重新发送请求或者说
+    });
+  };
+  return (
+    <div>
+      <p>{error.message}</p>
+      <button onClick={reload}>Try again</button>
+    </div>
+  );
 }
