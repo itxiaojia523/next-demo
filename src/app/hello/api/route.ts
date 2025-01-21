f1d653ebc17f95cb3c01e77e4b9634d4cb086584
xiaojia <chenminxin51@gmail.com>
2025-01-21 16:21:06 +0900

    Cookies in Route Handlers

--- src/app/hello/api/route.ts
+++ src/app/hello/api/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 //如果放在page同级，会覆盖page的路由
@@ -8,9 +8,16 @@ export async function GET(request: NextRequest) {
   // 方法2
   const headerLists = await headers();
   const requestHeader = headerLists.get("Authorization");
+
+  const theme = request.cookies.get("theme");
+  const cookieStore = await cookies();
+  cookieStore.set("theme2", "Light");
+  const theme2 = request.cookies.get("theme2");
+
   return new Response("<h1>Hello World!</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "set-cookie": "theme=Dark",
     },
     status: 200,
   });
