e2cc272802dfbba21473da6a87f7a80d78b5752d
xiaojia <chenminxin51@gmail.com>
2025-01-21 12:07:20 +0900

    Handling PATCH Requests:根据id更新comment

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -11,3 +11,19 @@ export async function GET(
   });
   return Response.json(coment);
 }
+
+export async function PATCH(
+  //不用的话，加一个_
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const body = await request.json();
+  const { text } = body;
+  const index = comments.findIndex((coment) => {
+    return coment.id === parseInt(id);
+  });
+  comments[index].text = text;
+
+  return Response.json(comments[index]);
+}
