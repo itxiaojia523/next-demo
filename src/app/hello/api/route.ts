7e90f965c964d17af4f2dc05bbf935636b40e132
xiaojia <chenminxin51@gmail.com>
2025-01-21 13:48:43 +0900

    Headers in Route Handlers

--- src/app/hello/api/route.ts
+++ src/app/hello/api/route.ts
@@ -1,4 +1,17 @@
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
 //如果放在page同级，会覆盖page的路由
-export async function GET() {
-  return new Response("Hello World!", { status: 200 });
+export async function GET(request: NextRequest) {
+  // 方法1
+  // const requestHeader = new Headers(request.headers);
+  // 方法2
+  const headerLists = await headers();
+  const requestHeader = headerLists.get("Authorization");
+  return new Response("<h1>Hello World!</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+    status: 200,
+  });
 }
