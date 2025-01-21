b79cf74822d590c9181abd6ed3e314c19ae021df
xiaojia <chenminxin51@gmail.com>
2025-01-21 11:41:14 +0900

    Handling GET Requests

---
+++ src/app/comments/data.ts
@@ -0,0 +1,5 @@
+export const comments = [
+  { id: 1, name: "John", comment: "This is a comment1" },
+  { id: 2, name: "John", comment: "This is a comment2" },
+  { id: 3, name: "John", comment: "This is a comment3" },
+];
