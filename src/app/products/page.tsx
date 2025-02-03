7e7b39a2094e4a8a85e6b46b13da1e8740707e01
xiaojia <chenminxin51@gmail.com>
2025-02-03 20:54:29 +0900

    Update Server Action : UI

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,7 @@
 import { getProducts } from "@/prisma-db";
 import Link from "next/link";
 
-type Product = {
+export type Product = {
   id: number;
   title: string;
   price: number;
@@ -14,15 +14,14 @@ export default async function Products() {
       <ul className="space-y-4 p-4">
         {products.map((product) => (
           <li key={product.id} className="p-4 border border-gray-200 rounded">
-            <h2 className="text-lg font-bold">{product.title}</h2>
+            <h2 className="text-lg font-bold">
+              <Link href={`/products/${product.id}`}> {product.title}</Link>
+            </h2>
             <p className="text-sm">{product.description}</p>
             <p className="text-sm">${product.price}</p>
           </li>
         ))}
       </ul>
-      <Link href="/products/1">p1</Link>
-      <Link href="/products/2">p2</Link>
-      <Link href="/products/3">p3</Link>
     </>
   );
 }
