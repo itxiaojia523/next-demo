913cbd61b4319eded4e32853fd126eacdf64ecb9
xiaojia <chenminxin51@gmail.com>
2025-02-04 10:10:33 +0900

    Protecting Routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/mock-users"]);
+const isProtectedRoute = createRouteMatcher(["/mock-users", "/user-profile"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
